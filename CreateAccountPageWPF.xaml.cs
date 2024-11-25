4039dcbb9bfe16252d54da9c7f63f23c2b8a9251
Grzybekk082 <dawid.grzybkowski@gmail.com>
2024-11-25 08:50:59 +0100

    CreateAccountPageWPF - aktualizacja i nadanie logiki

--- CreateAccountPageWPF.xaml.cs
+++ CreateAccountPageWPF.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUI_zaliczenie2025.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +21,34 @@ namespace GUI_zaliczenie2025
     /// </summary>
     public partial class CreateAccountPageWPF : UserControl
     {
+        string newName, newSurename, newLogin, newPassword;
         public CreateAccountPageWPF()
         {
             InitializeComponent();
+
         }
 
         private void Button_Send_Request_Click(object sender, RoutedEventArgs e)
         {
-        
+            
+            newName=textBoxName.Text;
+            newSurename=textBoxSurename.Text;
+            newLogin=textBoxLogin.Text;
+            newPassword=textBoxPassword.Text;
+
+            if (!AccountAcces.IsLoginFree(newName))
+            {
+                warningLabel.Content = "Podany login jest zajęty!";
+
+                textBoxLogin.Clear();
+            }
+            else
+            {
+                NewUsersRequests nu = new NewUsersRequests(newName, newSurename, newPassword, newLogin);
+                warningLabel.Content = "Prośba o utworzenie konta została wysłana do administratora.";
+
+                //AccountAcces ac = new AccountAcces(newName, newSurename, newPassword, newLogin);
+            }
         }
     }
 }
